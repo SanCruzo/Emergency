e25a97a19fb0f33402359d64d7f47f9d82854c92
riouta <78801832+riouta@users.noreply.github.com>
2025-04-28 10:26:24 +0200

    I set up the database postgresql, infos to be filled by db responsible

--- backend_code/backend/settings.py
+++ backend_code/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'our_db_name',       # <<-- Replace with PostgreSQL db name
+        'USER': 'our_db_user',        # <<-- PostgreSQL username
+        'PASSWORD': 'our_db_password',# <<-- PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
