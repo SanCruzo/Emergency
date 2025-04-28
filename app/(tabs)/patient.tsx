cf85ee8ec6e49557f37941034cf10f9cd6d94a04
davide259 <davide.pimazzoni02@gmail.com>
2025-04-28 16:17:15 +0200

    Update patient.tsx

--- app/(tabs)/patient.tsx
+++ app/(tabs)/patient.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useRouter } from 'expo-router';
 
 export default function PatientScreen() {
@@ -7,22 +7,33 @@ export default function PatientScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Patient</Text>
+        <View style={styles.topBar}>
+          <View style={styles.logoPlaceholder}>
+            <Image
+              source={require('../../assets/logo.png')}
+              style={styles.logoImage}
+              resizeMode="contain"
+            />
+          </View>
+          <Text style={styles.title}>Emergency Response</Text>
+        </View>
+
+            <Text style={styles.patient}>Insert Patient Info</Text>
       <View style={styles.buttonContainer}>
         {/* Button for patient with ID */}
         <TouchableOpacity
           style={styles.button}
-          onPress={() => router.push('/patientAfterRegister?hasID=true')}
+          onPress={() => router.push('/InsertPatient')}
         >
           <Text style={styles.buttonText}>
-            Registration patient (via Tessera Sanitaria) or Carta d’identità (NFC?)
+            Registration patient (via Tessera Sanitaria)
           </Text>
         </TouchableOpacity>
 
         {/* Button for patient without ID */}
         <TouchableOpacity
           style={styles.button}
-          onPress={() => router.push('/patientAfterRegister?hasID=false')}
+          onPress={() => router.push('/InsertPatientNO_ID')}
         >
           <Text style={styles.buttonText}>Registration patient (without any ID)</Text>
         </TouchableOpacity>
@@ -32,30 +43,61 @@ export default function PatientScreen() {
         must complete an electronic self-certification attesting the need to consult the file for
         treatment purposes in an emergency situation.
       </Text>
+
+      <View style={styles.bottomBar}>
+          <TouchableOpacity style={styles.logoutButton} onPress={() => console.log('Logged user')}>
+            <Text style={styles.logoutText}>Logged user: John Doe</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.logoutButton} onPress={() => console.log('Logout button pressed')}>
+            <Text style={styles.logoutTexùt}>LOGOUT</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+      flex: 1,
+      flexDirection: 'column',
+      paddingHorizontal: 40,
+      paddingVertical: 30,
+      backgroundColor: '#fff',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+  topBar: {
+      flexDirection: 'row',
+      backgroundColor: '#00FFFF',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: 100,
+      width: '100%',
+      borderRadius: 15,
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+    },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10,
   },
+  patient:{
+      fontSize: 24,
+      fontWeight: 'bold',
+      alignItems: 'center',
+      paddingVertical: 70,
+      marginBottom: -25,
+      justifyContent: 'center',
+    },
   buttonContainer: {
     width: '100%',
     alignItems: 'center',
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#90caf9',
+    backgroundColor: '#00B7EB',
     borderRadius: 10,
     padding: 20,
     marginVertical: 10,
@@ -68,9 +110,42 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   footerText: {
-    fontSize: 14,
-    textAlign: 'center',
-    color: '#555',
-    marginTop: 20,
-  },
+      fontSize: 14,
+      textAlign: 'center',
+      color: '#555',
+      marginTop: 20,
+    },
+    logoPlaceholder: {
+      width: 100,
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#eee',
+      borderRadius: 25,
+      overflow: 'hidden',
+    },
+    logoImage: {
+      width: '100%',
+      height: '100%',
+    },
+    bottomBar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: 60,
+      width: '100%',
+      backgroundColor: '#FFFFFF',
+      borderRadius: 10,
+      paddingHorizontal: 20,
+      marginTop: 20,
+    },
+    logoutButton: {
+      backgroundColor: '#eee',
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      borderRadius: 5,
+    },
+    logoutText: {
+      fontWeight: 'bold',
+    },
 });
