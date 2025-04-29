69aaf034d1383575540c4f5285559b19a6d6ec4c
Kidist A. Kassa <kidsitandarge.kassa@studenti.unipd.it>
2025-04-29 22:44:29 +0200

    database updated with new tables

--- database/emer.sql
+++ database/emer.sql
@@ -24,65 +24,35 @@ ALTER TABLE IF EXISTS mob_er."user"
 
 
 
+-- Table: mob_er.Livechat
 
--- Table: mob_er.messages
+-- DROP TABLE IF EXISTS mob_er."Livechat";
 
--- DROP TABLE IF EXISTS mob_er.messages;
-
-CREATE TABLE IF NOT EXISTS mob_er.messages
+CREATE TABLE IF NOT EXISTS mob_er."Livechat"
 (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     sender_id uuid NOT NULL,
     receiver_id uuid NOT NULL,
-    emergency_id uuid,
-    encrypted_message text COLLATE pg_catalog."default" NOT NULL,
-    "timestamp" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT messages_pkey PRIMARY KEY (id),
-    CONSTRAINT messages_emergency_id_fkey FOREIGN KEY (emergency_id)
-        REFERENCES mob_er.emergencies (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT messages_receiver_id_fkey FOREIGN KEY (receiver_id)
-        REFERENCES mob_er."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT messages_sender_id_fkey FOREIGN KEY (sender_id)
-        REFERENCES mob_er."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS mob_er.messages
-    OWNER to "ER";
-
-
--- Table: mob_er.emergencies
-
--- DROP TABLE IF EXISTS mob_er.emergencies;
-
-CREATE TABLE IF NOT EXISTS mob_er.emergencies
-(
-    id uuid NOT NULL DEFAULT gen_random_uuid(),
-    status character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    created_by uuid NOT NULL,
-    location_lat numeric(9,6) NOT NULL,
-    location_long numeric(9,6) NOT NULL,
+    message text COLLATE pg_catalog."default" NOT NULL,
+    status character varying(10) COLLATE pg_catalog."default" DEFAULT 'sent'::character varying,
+    edited_at timestamp without time zone,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT emergencies_pkey PRIMARY KEY (id),
-    CONSTRAINT emergencies_created_by_fkey FOREIGN KEY (created_by)
+    CONSTRAINT "Livechat_pkey" PRIMARY KEY (id),
+    CONSTRAINT "Livechat_receiver_id_fkey" FOREIGN KEY (receiver_id)
         REFERENCES mob_er."user" (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT emergencies_status_check CHECK (status::text = ANY (ARRAY['full'::character varying, 'available'::character varying]::text[]))
+    CONSTRAINT "Livechat_sender_id_fkey" FOREIGN KEY (sender_id)
+        REFERENCES mob_er."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "Livechat_status_check" CHECK (status::text = ANY (ARRAY['sent'::character varying, 'delivered'::character varying, 'read'::character varying]::text[]))
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS mob_er.emergencies
+ALTER TABLE IF EXISTS mob_er."Livechat"
     OWNER to "ER";
-
 
 -- Table: mob_er.ambulance
 
@@ -106,4 +76,49 @@ CREATE TABLE IF NOT EXISTS mob_er.ambulance
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS mob_er.ambulance
+    OWNER to "ER";
+-- Table: mob_er.patient
+
+-- DROP TABLE IF EXISTS mob_er.patient;
+
+CREATE TABLE IF NOT EXISTS mob_er.patient
+(
+    patientid uuid NOT NULL DEFAULT gen_random_uuid(),
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    middlename character varying(50) COLLATE pg_catalog."default",
+    surname character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    age integer,
+    sex character(1) COLLATE pg_catalog."default",
+    CONSTRAINT patient_pkey PRIMARY KEY (patientid),
+    CONSTRAINT patient_age_check CHECK (age >= 0),
+    CONSTRAINT patient_sex_check CHECK (sex = ANY (ARRAY['M'::bpchar, 'F'::bpchar]))
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS mob_er.patient
+    OWNER to "ER";
+
+    -- Table: mob_er.vital_signs
+
+-- DROP TABLE IF EXISTS mob_er.vital_signs;
+
+CREATE TABLE IF NOT EXISTS mob_er.vital_signs
+(
+    patientid uuid NOT NULL,
+    heartrate integer,
+    bloodpressure character varying(20) COLLATE pg_catalog."default",
+    temperature numeric(4,2),
+    respirationrate integer,
+    recordedat timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT vital_signs_pkey PRIMARY KEY (patientid),
+    CONSTRAINT vital_signs_patientid_fkey FOREIGN KEY (patientid)
+        REFERENCES mob_er.patient (patientid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS mob_er.vital_signs
     OWNER to "ER";
