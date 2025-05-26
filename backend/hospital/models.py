105ef1c776daa01793e7576dc3f4e6b476f2bbf8
riouta <78801832+riouta@users.noreply.github.com>
2025-05-27 00:18:37 +0200

    save localchanges before pulling

--- backend/hospital/models.py
+++ backend/hospital/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from users.models import User
 
 class Hospital(models.Model):
     name = models.CharField(max_length=100)
     hosp_lat = models.DecimalField(max_digits=9, decimal_places=6)
     hosp_long = models.DecimalField(max_digits=9, decimal_places=6)
     address = models.TextField()
+    staff = models.ManyToManyField(User, limit_choices_to={'role': 'hospital'}, blank=True) #allows to assign multiple staff members (users with role hospital) to a hospital
 
     def __str__(self):
         return self.name
