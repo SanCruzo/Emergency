f18b17e4cca2723b254aa939caf7dfed411ed189
riouta <78801832+riouta@users.noreply.github.com>
2025-05-27 13:25:15 +0200

    implemented RBAC for hospital and ambulance staff

--- backend/hospital/models.py
+++ backend/hospital/models.py
@@ -6,7 +6,7 @@ class Hospital(models.Model):
     hosp_lat = models.DecimalField(max_digits=9, decimal_places=6)
     hosp_long = models.DecimalField(max_digits=9, decimal_places=6)
     address = models.TextField()
-    staff = models.ManyToManyField(User, limit_choices_to={'role': 'hospital'}, blank=True) #allows to assign multiple staff members (users with role hospital) to a hospital
+    #staff = models.ManyToManyField(User, limit_choices_to={'role': 'hospital'}, blank=True) #allows to assign multiple staff members (users with role hospital) to a hospital
 
     def __str__(self):
         return self.name
--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import uuid
+from users.models import User
+from hospital.models import Hospital
+
 
 class Patient(models.Model):
     SYMPTOM_CATEGORIES = [
@@ -21,16 +24,20 @@ class Patient(models.Model):
 
     patient_id = models.CharField(max_length=100, blank=True, null=True)
     hasID = models.BooleanField(default=False)  # True if patient has ID, False if system generated
+    name = models.CharField(max_length=100)
     symptoms = models.JSONField(default=list, blank=True, null=True)  
     triage_code = models.CharField(max_length=15, choices=TRIAGE_CHOICES, blank=True, null=True)
     is_active = models.BooleanField(default=True, blank=True, null=True) #whether this case is active
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_patients')
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, null=True, blank=True)
+    #hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     # hospital = models.ForeignKey(Hospital, on_delete=models.SET_NULL, null=True, related_name='patients')
     # ambulance = models.ForeignKey(Ambulance, on_delete=models.SET_NULL, null=True, blank=True, related_name='patients')
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     
     #extra infos for patient without ID
     gender = models.CharField(max_length=10, blank=True, null=True)
-    age_group = models.CharField(max_length=20, blank=True, null=True)
+    approximate_age = models.CharField(max_length=20, blank=True, null=True)
     height = models.CharField(max_length=20, blank=True, null=True)
     weight = models.CharField(max_length=20, blank=True, null=True)
     ethnicity = models.CharField(max_length=20, blank=True, null=True) 
